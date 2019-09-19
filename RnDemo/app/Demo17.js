61b32992f588c8371fbbc176842194728125435e
千剑 <969308461@qq.com>
2019-09-19 11:36:18 +0800

    Demo17 1.react-native-pull-to-refresh 测试

---
+++ RnDemo/app/Demo17.js
@@ -0,0 +1,96 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+
+import PTRView from 'react-native-pull-to-refresh'
+
+export default class Demo17 extends Component {
+  constructor(param) {
+    super(param);
+    this.state = {
+      cards: [1, 2, 3]
+    }
+    this._refresh = this._refresh.bind(this);
+
+  }
+  _refresh () {
+    // you must return Promise everytime
+    return new Promise((resolve) => {
+      setTimeout(()=>{
+        // some refresh process should come here
+        this.setState({cards: this.state.cards.concat([this.state.cards.length + 1])})
+        resolve(); 
+      }, 500)
+    })
+  }
+  render() {
+    return (
+      <View style={{flex:1}}>
+      
+        <PTRView
+          style={{backgroundColor:'#F5FCFF'}}
+          onRefresh={this._refresh}
+        >
+          <View style={styles.container}>
+            <Text style={styles.welcome}> Pull Me!😸 </Text>
+            {this.state.cards.map((el, i) => (
+              <View style={styles.card} key={i}>
+                <Text style={styles.card__text}>Card: {el}</Text>
+              </View>
+            ))}
+          </View>
+        </PTRView>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  header: {
+    height: 60,
+    borderColor: '#f9f9f9',
+    backgroundColor: '#2196F3',
+    borderBottomWidth: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  headerText: {
+    color: '#fff',
+    fontSize: 20,
+    lineHeight: 40,
+  },
+  card: {
+    flex: 1,
+    borderColor: '#fafafa',
+    backgroundColor: '#2196F3',
+    borderWidth: 2,
+    borderRadius: 3,
+    margin: 5,
+  },
+  card__text: {
+    color: '#fafafa',
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  }
+});
---
+++ RnDemo/index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import Demo17 from "./app/Demo17";
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => Demo17);
